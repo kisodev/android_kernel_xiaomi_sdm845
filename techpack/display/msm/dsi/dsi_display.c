9966e4e8604f253916179e4ccbe5d5be3c93f1e2
duckyduckg <duckyduckg65@gmail.com>
2025-04-08 15:25:18 +0500

    techpack: display: fix uninitialized dev pointer in dsi_display_set_power
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -1264,6 +1264,7 @@ int dsi_display_set_power(struct drm_connector *connector,
 		return -EINVAL;
 	}
 
+	dev = display->drm_dev;
 	if (!dev) {
 		DSI_ERR("Invalid DRM device\n");
 		return -EINVAL;
