940fc1a8e98e85f4e3c950adbe1e83bba979850d
duckyduckg <duckyduckg65@gmail.com>
2025-05-25 16:39:53 +0500

    kernel: Upgrade to SUSFS 1.5.7 hooks
    - from
    https://github.com/ShirkNeko/susfs4ksu/tree/kernel-4.19
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -20,12 +20,12 @@
 #include <linux/uaccess.h>
 #include <linux/pkeys.h>
 #include <linux/mm_inline.h>
+#include <linux/ctype.h>
+
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 #include <linux/susfs_def.h>
 #endif
-
-#include <linux/ctype.h>
-
+ 
 #include <asm/elf.h>
 #include <asm/tlb.h>
 #include <asm/tlbflush.h>
@@ -352,10 +352,6 @@ static void show_vma_header_prefix(struct seq_file *m,
 	seq_putc(m, ' ');
 }
 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-extern void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
-#endif
-
 #ifdef CONFIG_KSU
 static void show_vma_header_prefix_fake(struct seq_file *m,
 					unsigned long start, unsigned long end,
@@ -373,6 +369,10 @@ static void show_vma_header_prefix_fake(struct seq_file *m,
 			pgoff,
 			MAJOR(dev), MINOR(dev), ino);
 }
+#endif
+
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+extern void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
 #endif
 
 static void
