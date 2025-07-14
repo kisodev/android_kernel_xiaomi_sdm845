117e1252cd2355e5414d1215bb7913237a7bf6e0
duckyduckG <duckyduckg65@gmail.com>
2025-07-15 02:40:22 +0500

    techpack: display: dsi_panel: export symbols for dt2w
    * required for touch drivers
    
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -391,6 +391,7 @@ static int dsi_panel_gpio_release(struct dsi_panel *panel)
 	if (g_panel)
 		g_panel->panel_reset_skip = enable;
 }
+EXPORT_SYMBOL(drm_panel_reset_skip_enable);
 
  void drm_dsi_ulps_enable(bool enable)
 {
@@ -399,6 +400,7 @@ static int dsi_panel_gpio_release(struct dsi_panel *panel)
 		g_panel->ulps_suspend_enabled = enable;
 	}
 }
+EXPORT_SYMBOL(drm_dsi_ulps_enable);
 
 int dsi_panel_trigger_esd_attack(struct dsi_panel *panel)
 {
@@ -430,6 +432,7 @@ int dsi_panel_trigger_esd_attack(struct dsi_panel *panel)
 	if (g_panel)
 		g_panel->ulps_suspend_enabled = enable;
 }
+EXPORT_SYMBOL(drm_dsi_ulps_suspend_enable);
 
 static int dsi_panel_reset(struct dsi_panel *panel)
 {
