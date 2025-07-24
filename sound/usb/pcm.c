cc9e53a153d22228a148bf42e3477250fc76b9e2
duckyduckG <duckyduckg65@gmail.com>
2025-07-24 22:30:05 +0500

    sound: usb: pcm: Import xiaome changes
    * from dipper-q-oss

--- sound/usb/pcm.c
+++ sound/usb/pcm.c
@@ -568,7 +568,9 @@ static int set_sync_endpoint(struct snd_usb_substream *subs,
 
 	return 0;
 }
-
+// xiaomi change
+extern void kick_usbpd_vbus_sm(void);
+// xiaomi change
 /*
  * find a matching format and set up the interface
  */
@@ -622,6 +624,10 @@ static int set_format(struct snd_usb_substream *subs, struct audioformat *fmt)
 			dev_err(&dev->dev,
 				"%d:%d: usb_set_interface failed (%d)\n",
 				fmt->iface, fmt->altsetting, err);
+			if ((0x2717 == USB_ID_VENDOR(subs->stream->chip->usb_id))&&(0x3801 == USB_ID_PRODUCT(subs->stream->chip->usb_id)))
+			{
+				kick_usbpd_vbus_sm();
+			}
 			return -EIO;
 		}
 		dev_dbg(&dev->dev, "setting usb interface %d:%d\n",
